4b9c1207ef3f0a41805f02009881a3440dcc0327
Geoff Larsen <github@glarsen.site>
2020-08-19 11:55:30 -0600

    Update aeris.py

--- nodes/aeris.py
+++ nodes/aeris.py
@@ -368,7 +368,7 @@ class Controller(polyinterface.Controller):
             #  convert these.
             
             #J = datetime.datetime.fromtimestamp(epoch).timetuple().tm_yday
-            J = 230
+            J = 232
 
             Tmin = rd['temp'][self.tag['temp_avg']]
             Tmax = rd['temp'][self.tag['temp_avg']]
@@ -383,7 +383,7 @@ class Controller(polyinterface.Controller):
             #else:
             #    Ws = et3.kph2ms(Ws)
 
-            et0 = et3.evapotranspriation(Tmax, Tmin, None, Ws, float(self.params.get('Elevation')), rd['rh'][self.tag['humidity_max_summ']], rd['rh'][self.tag['humidity_min_summ']], 51.12, float(self.params.get('Plant Type')), J)
+            et0 = et3.evapotranspriation(Tmax, Tmin, None, kph2ms(Ws), float(self.params.get('Elevation')), rd['rh'][self.tag['humidity_max_summ']], rd['rh'][self.tag['humidity_min_summ']], 51.12, float(self.params.get('Plant Type')), J)
             #if self.units == 'metric' or self.units == 'si' or self.units.startswith('m'):
             self.update_driver('GV20', round(et0, 2))
             #else:
