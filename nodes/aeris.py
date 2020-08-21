951aebd6b2360a81b24414c7c347231796e7420a
Geoff Larsen <github@glarsen.site>
2020-08-21 11:41:43 -0600

    Update aeris.py

--- nodes/aeris.py
+++ nodes/aeris.py
@@ -389,7 +389,7 @@ class Controller(polyinterface.Controller):
             else:
                 LOGGER.info('Conversion of wind speed required')
                 Ws = et3.kph2ms(Ws)
-            LOGGER.debug('Setting Ws: %f m/s' % (Ws)))
+            LOGGER.debug('Setting Ws: %f m/s' % (Ws))
             et0 = et3.evapotranspriation(Tmax, Tmin, None, Ws, float(self.params.get('Elevation')), rd['rh'][self.tag['humidity_max_summ']], rd['rh'][self.tag['humidity_min_summ']], 51.12, float(self.params.get('Plant Type')), J, Tavg)
             #if self.units == 'metric' or self.units == 'si' or self.units.startswith('m'):
             self.update_driver('GV20', round(et0, 2))
